f249fc942fedd9ab7de97edd93516bdbf676a1d4
Rohit Manohar <manoharrohit372@gmail.com>
2026-01-09 00:03:53 +0530

    set up variables

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const socket = require("socket.io");
 const http = require("http");
-const {chess} = require("chess.js");
+const { Chess } = require("chess.js");
+const path = require("path");
 
 const app = express();
-
 const server = http.createServer(app);
 const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "w";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Online Chess Game" });
+});
+
+io.on("connection", function (uniquesocket) {
+    console.log("User connected")
+
+    uniquesocket.on("disconnected", function(){
+        console.log("disconnected");
+    });
+
+});
+
+server.listen(3000, function ()  {
+    console.log("listening on port 3000");
+});
